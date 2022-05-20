902a7beeb0f3f8298692cba014045c6b2a02dbda
Luc Briand <luc.briand35@gmail.com>
2022-05-20 15:20:36 +0200

    Small script to auto-update plots

---
+++ update_plot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+display_cmd="gio open"
+last_update_file_path=./plots/last_update
+
+# Set initial time of file
+if [ -e $last_update_file_path ]
+then
+    last_update_timestamp=`stat -c %Z $last_update_file_path`
+else
+    last_update_timestamp=0
+    touch $last_update_file_path
+fi
+
+# If the timestamp changes, read the file to get the path to the updated plot and update its display
+while true    
+do
+   file_timestamp=`stat -c %Z $last_update_file_path`
+
+   if [[ "$file_timestamp" != "$last_update_timestamp" ]]
+   then    
+       last_update_timestamp=$file_timestamp
+       updated_plot=`echo $(<$last_update_file_path)`
+       echo plot updated: $updated_plot
+       $display_cmd $updated_plot
+   fi
+   sleep 0.5
+done
