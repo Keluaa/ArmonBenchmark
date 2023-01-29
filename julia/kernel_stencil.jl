12ac8c66f33adeb570bbf51df02656e0fa22ada3
Keluaa <Lukeb35@gmail.com>
2023-01-29 22:09:08 +0100

    Kernels stencils WIP

--- julia/kernel_stencil.jl
+++ julia/kernel_stencil.jl
@@ -1,82 +1,128 @@
 
-mutable struct ImprintingArray{T, N} <: AbstractArray{T, N}
+using StaticArrays
+
+
+struct ImprintingArray{T, N} <: AbstractArray{T, N}
     dims::NTuple{N, Int}
-    min_get::Union{NTuple{N, Int}, Missing}
-    max_get::Union{NTuple{N, Int}, Missing}
-    min_set::Union{NTuple{N, Int}, Missing}
-    max_set::Union{NTuple{N, Int}, Missing}
+    offset::NTuple{N, Int}
+    min_get::MVector{N, Int}
+    max_get::MVector{N, Int}
+    min_set::MVector{N, Int}
+    max_set::MVector{N, Int}
 end
 
 
-ImprintingArray(::Type{T}) where T = ImprintingArray{T, 1}((1,), missing, missing, missing, missing)
-ImprintingArray(::Type{T}, dims::Int...) where T = ImprintingArray(T, dims)
-ImprintingArray(::Type{T}, dims::NTuple{N, Int}) where {T, N} = ImprintingArray{T, N}(dims, missing, missing, missing, missing)
+ImprintingArray{T}(dims::Int...) where T = ImprintingArray{T, length(dims)}(tuple(dims...))
+ImprintingArray{T}(dims::NTuple{N, Int}) where {T, N} = ImprintingArray{T, N}(dims)
+ImprintingArray{T}(dims::NTuple{N, Int}, offset::NTuple{N, Int}) where {T, N} = ImprintingArray{T, N}(dims, offset)
+ImprintingArray{T, N}(dims::NTuple{N, Int}) where {T, N} = ImprintingArray{T, N}(dims, ntuple(Returns(0), Val(N)))
+ImprintingArray{T, N}(dims::NTuple{N, Int}, offset::NTuple{N, Int}) where {T, N} =
+    ImprintingArray{T, N}(
+        dims, offset,
+        MVector{N, Int}(ntuple(Returns(typemax(Int)), Val(N))),
+        MVector{N, Int}(ntuple(Returns(typemin(Int)), Val(N))),
+        MVector{N, Int}(ntuple(Returns(typemax(Int)), Val(N))),
+        MVector{N, Int}(ntuple(Returns(typemin(Int)), Val(N)))
+    )
 
-Base.size(arr::ImprintingArray{T, N}) where {T, N} = arr.dims
+Base.size(arr::ImprintingArray) = arr.dims
 Base.length(arr::ImprintingArray) = prod(arr.dims)
 
-Base.checkbounds(Bool, ::ImprintingArray, I...) = true
+Base.checkbounds(::Type{Bool}, ::ImprintingArray, I...) = true
 
 
-idx_to_multi_dims(i::Int, _::NTuple{1, Int}) = i
-
-function idx_to_multi_dims(i::Int, dims::NTuple{2, Int})
-    iy, ix = divrem(i-1, dims[1])
-    return ix+1, iy+1
+function Base.copy(arr::ImprintingArray{T, N}) where {T, N}
+    ImprintingArray{T, N}(
+        arr.dims, arr.offset,
+        copy(arr.min_get),
+        copy(arr.max_get),
+        copy(arr.min_set),
+        copy(arr.max_set)
+    )
 end
 
-function idx_to_multi_dims(i::Int, dims::NTuple{3, Int})
+
+idx_to_multi_dims(i::Int, _::NTuple{1, Int}, offset::NTuple{1, Int}) = i + offset[1]
+
+function idx_to_multi_dims(i::Int, dims::NTuple{2, Int}, offset::NTuple{2, Int})
+    iy, ix = divrem(i-1, dims[1])
+    return ix + 1 + offset[1], iy + 1 + offset[2]
+end
+
+function idx_to_multi_dims(i::Int, dims::NTuple{3, Int}, offset::NTuple{3, Int})
     iy, ix = divrem(i-1, dims[1])
     iz, iy = divrem(iy, dims[2])
-    return ix+1, iy+1, iz+1
+    return ix + 1 + offset[1], iy + 1 + offset[2], iz + 1 + offset[3]
 end
 
 
 function Base.getindex(arr::ImprintingArray{T, N}, i::Int) where {T, N}
-    indexes = idx_to_multi_dims(i, arr.dims)
-    arr.min_get = min.(indexes, @coalesce(arr.min_get, indexes))
-    arr.max_get = max.(indexes, @coalesce(arr.max_get, indexes))
+    indexes = idx_to_multi_dims(i, arr.dims, arr.offset)
+    arr.min_get .= min.(arr.min_get, indexes)
+    arr.max_get .= max.(arr.max_get, indexes)
     return zero(T)
 end
 
 
 function Base.setindex!(arr::ImprintingArray{T, N}, _, i::Int) where {T, N}
-    indexes = idx_to_multi_dims(i, arr.dims)
-    arr.min_set = min.(indexes, @coalesce(arr.min_set, indexes))
-    arr.max_set = max.(indexes, @coalesce(arr.max_set, indexes))
+    indexes = idx_to_multi_dims(i, arr.dims, arr.offset)
+    arr.min_set .= min.(arr.min_set, indexes)
+    arr.max_set .= max.(arr.max_set, indexes)
     return zero(T)
 end
 
 
 function Base.union(arr1::ImprintingArray{T, N}, arr2::ImprintingArray{T, N}) where {T, N}
-    any(arr1.dims .!= arr2.dims) && throw(DimensionMismatch("$(arr1.dims) != $(arr2.dims)"))
+    any(arr1.dims   .!= arr2.dims  ) && throw(DimensionMismatch("$(arr1.dims) != $(arr2.dims)"))
+    any(arr1.offset .!= arr2.offset) && throw(DimensionMismatch("$(arr1.offset) != $(arr2.offset) (offset mismatch)"))
     return ImprintingArray{T, N}(
-        arr1.dims,
-        min.(@coalesce(arr1.min_get, arr2.min_get), @coalesce(arr2.min_get, arr1.min_get)),
-        max.(@coalesce(arr1.max_get, arr2.max_get), @coalesce(arr2.max_get, arr1.max_get)),
-        min.(@coalesce(arr1.min_set, arr2.min_set), @coalesce(arr2.min_set, arr1.min_set)),
-        max.(@coalesce(arr1.max_set, arr2.max_set), @coalesce(arr2.max_set, arr1.max_set))
+        arr1.dims, arr1.offset,
+        MVector{N, Int}(min.(arr1.min_get, arr2.min_get)),
+        MVector{N, Int}(max.(arr1.max_get, arr2.max_get)),
+        MVector{N, Int}(min.(arr1.min_set, arr2.min_set)),
+        MVector{N, Int}(max.(arr1.max_set, arr2.max_set))
     )
 end
 
 
-function Base.union(arr1::ImprintingArray{T, N}, arrays::ImprintingArray{T, N}...) where {T, N}
-    arr = arr1
-    for arr2 in arrays
-        arr = union(arr, arr2)
-    end
-    return arr
+function Base.union!(arr1::ImprintingArray{T, N}, arr2::ImprintingArray{T, N}) where {T, N}
+    any(arr1.dims   .!= arr2.dims  ) && throw(DimensionMismatch("$(arr1.dims) != $(arr2.dims)"))
+    any(arr1.offset .!= arr2.offset) && throw(DimensionMismatch("$(arr1.offset) != $(arr2.offset) (offset mismatch)"))
+    arr1.min_get .= min.(arr1.min_get, arr2.min_get)
+    arr1.max_get .= max.(arr1.max_get, arr2.max_get)
+    arr1.min_set .= min.(arr1.min_set, arr2.min_set)
+    arr1.max_set .= max.(arr1.max_set, arr2.max_set)
+    return arr1
+end
+
+
+Base.union(arrays::ImprintingArray{T, N}...) where {T, N} = foldl(union, arrays)
+Base.union!(arrays::ImprintingArray{T, N}...) where {T, N} = foldl(union!, arrays)
+
+
+was_written(arr::ImprintingArray) = !any(arr.min_set .== typemax(Int))
+was_read(arr::ImprintingArray) = !any(arr.min_get .== typemax(Int))
+was_accessed(arr::ImprintingArray) = was_written(arr) || was_read(arr)
+
+function reset_gets(arr::ImprintingArray{T, N}) where {T, N}
+    arr.min_get .= ntuple(Returns(typemax(Int)), Val(N))
+    arr.max_get .= ntuple(Returns(typemin(Int)), Val(N))
+end
+
+function reset_sets(arr::ImprintingArray{T, N}) where {T, N}
+    arr.min_set .= ntuple(Returns(typemax(Int)), Val(N))
+    arr.max_set .= ntuple(Returns(typemin(Int)), Val(N))
 end
 
 
 function Base.show(io::IO, arr::ImprintingArray{T, N}) where {T, N}
-    if ismissing(arr.min_get)
+    if !was_read(arr)
         get_range = "∅"
     else
         get_range = join(map(i -> arr.min_get[i]:arr.max_get[i], 1:N), ", ")
     end
 
-    if ismissing(arr.min_set)
+    if !was_written(arr)
         set_range = "∅"
     else
         set_range = join(map(i -> arr.min_set[i]:arr.max_set[i], 1:N), ", ")
@@ -121,32 +167,32 @@ function kernel_stencil(params::ArmonParameters, kernel::Function;
     cpu_kernel_func = getproperty(kernel_module, kernel_name)
     cpu_kernel = first(methods(cpu_kernel_func))
 
-    # Get a DataType instance of the signature of the method
+    # Get a DataType instance of the signature of the method
     args_data_type = Base.unwrap_unionall(cpu_kernel.sig)
     args_types = args_data_type.types
-    args_types = args_types[2:end]  # Skip the method type
+    args_types = args_types[2:end]  # Skip the method type
 
-    # Get low level info about the method declaration
+    # Get low level info about the method declaration
     _, decls, _, _ = Base.arg_decl_parts(cpu_kernel)
     args_names = decls[2:end]
 
-    # Ignore the first 'params' argument
+    # Ignore the first 'params' argument
     args_types = args_types[2:end]
     args_names = args_names[2:end]
 
-    # Ignore the SIMD and threading switches arguments
+    # Ignore the SIMD and threading switches arguments
     args_types = args_types[1:end-2]
     args_names = args_names[1:end-2]
 
     kernel_ranges = []
     if args_names[1][1] == "loop_range"
-        # kernel with 1D indexing
+        # kernel with 1D indexing
         push!(kernel_ranges, 1:1)
 
         args_types = args_types[2:end]
         args_names = args_names[2:end]
     else
-        # kernel with 2D indexing
+        # kernel with 2D indexing
         push!(kernel_ranges, 1:1, 1:1)
 
         args_types = args_types[3:end]
@@ -155,7 +201,7 @@ function kernel_stencil(params::ArmonParameters, kernel::Function;
 
     imprinting_arrays = Dict{Symbol, Any}()
 
-    # Build the array of arguments to pass to the kernel. All dynamic arrays types are replaced with 
+    # Build the array of arguments to pass to the kernel. All dynamic arrays types are replaced with
     # ImprintingArray.
     params_fields = fieldnames(typeof(params))
     args_instances = []
@@ -177,12 +223,12 @@ function kernel_stencil(params::ArmonParameters, kernel::Function;
 
         if arg_type isa TypeVar
             if arg_type.ub isa UnionAll
-                # arg_type = V <: AbstractArray{T, N}
+                # arg_type = V <: AbstractArray{T, N}
                 array_type = Base.unwrap_unionall(arg_type.ub)
                 is_arg_abstract_array = array_type.name.name == :AbstractArray
             else
                 if haskey(type_args, arg_type.name) && type_args[arg_type.name] == AbstractArray
-                    # The type is not annotated as an abstract array ('V <: AbstractArray') but the
+                    # The type is not annotated as an abstract array ('V <: AbstractArray') but the
                     # user says so here.
                     array_type = Base.unwrap_unionall(AbstractArray)
                     is_arg_abstract_array = true
@@ -193,16 +239,16 @@ function kernel_stencil(params::ArmonParameters, kernel::Function;
 
             if is_arg_abstract_array
                 if array_type.parameters[1] isa TypeVar
-                    # AbstractArray{T, N} -> Variadic type, by default we choose T=Float64
+                    # AbstractArray{T, N} -> Variadic type, by default we choose T=Float64
                     var_type_name = array_type.parameters[1].name
                     if haskey(type_args, var_type_name)
-                        # This specific eltype is specified by the user
+                        # This specific eltype is specified by the user
                         arg_eltype = type_args[var_type_name]
                     else
                         arg_eltype = Float64
                     end
                 else
-                    # AbstractArray{<fixed type>, N}
+                    # AbstractArray{<fixed type>, N}
                     arg_eltype = array_type.parameters[1]
                 end
                 imprinting_arrays[name] = ImprintingArray(arg_eltype, params.global_grid)
@@ -217,8 +263,8 @@ function kernel_stencil(params::ArmonParameters, kernel::Function;
         end
 
         if isnothing(arg_val)
-            # Try to use the default value of the type: 0 for numbers, default constructor for other
-            # types
+            # Try to use the default value of the type: 0 for numbers, default constructor for other
+            # types
             if arg_type <: Number
                 arg_val = zero(arg_type)
             else
--- julia/tests/stencil_coverage.jl
+++ julia/tests/stencil_coverage.jl
@@ -100,3 +100,189 @@ function get_all_kernel_stencils()
 
     return kernel_stencils
 end
+
+
+function imprinting_data(params::ArmonParameters{T}) where T
+    dims = (params.row_length, params.col_length)
+    offsets = (-params.nghost, -params.nghost)
+    return Armon.ArmonData{ImprintingArray{T, 2}}(
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}(dims, offsets),
+        ImprintingArray{T, 2}((params.comm_array_size, 1))
+    )
+end
+
+
+function build_kernel_hiearchy()
+    params = ArmonParameters(;
+        :ieee_bits => sizeof(Float64)*8,
+        :test => :Sod, :scheme => :GAD, :projection => :euler_2nd, :riemann_limiter => :minmod,
+        :nghost => 5, :nx => 1, :ny => 1,
+        :cfl => 0,
+        :silent => 5, :write_output => false, :measure_time => false,
+        :use_MPI => false,
+        :single_comm_per_axis_pass => false, :async_comms => false,
+        :use_simd => false, :use_threading => false,
+    )
+    host_array = Vector{Float64}()
+
+    dt = params.Dt
+
+    steps = [
+        (d, drs) -> (Armon.dtCFL, Armon.dtCFL(params, d, dt)),
+        (d, drs) -> (Armon.update_EOS!, Armon.update_EOS!(params, d, Armon.full_domain(drs), :full)),
+        (d, drs) -> (Armon.boundaryConditions!, Armon.boundaryConditions!(params, d, host_array, params.current_axis)),
+        (d, drs) -> (Armon.numericalFluxes!, Armon.numericalFluxes!(params, d, dt, drs, :full)),
+        (d, drs) -> (Armon.cellUpdate!, Armon.cellUpdate!(params, d, dt, drs)),
+        (d, drs) -> (Armon.projection_remap!, Armon.projection_remap!(params, d, host_array, dt)),
+    ]
+
+    axes_stencils = Dict{Armon.Axis, Dict{Symbol, Armon.ArmonData}}()
+
+    for axis in (Armon.X_axis, Armon.Y_axis)
+        steps_stencil = Dict{Symbol, Armon.ArmonData}()
+        axes_stencils[axis] = steps_stencil
+
+        Armon.update_axis_parameters(params, axis)
+        domain_ranges = Armon.compute_domain_ranges(params)
+
+        for step in steps
+            data = imprinting_data(params)
+            func, _ = step(data, domain_ranges)
+            step_name = typeof(func).name.name
+            steps_stencil[step_name] = data
+        end
+    end
+
+    for (axis, steps_stencil) in axes_stencils
+        println("\n$axis:")
+        for (step, data) in steps_stencil
+            println("  $step:")
+            for field in fieldnames(typeof(data))
+                array = getproperty(data, field)
+                !was_accessed(array) && continue
+                println("     $field:\t", array)
+            end
+        end
+    end
+
+    axes_stencils
+end
+
+
+function print_data_stencil(data::Armon.ArmonData{<:ImprintingArray}; indent="")
+    for field in fieldnames(typeof(data))
+        array = getproperty(data, field)
+        !was_accessed(array) && continue
+        println("$indent$field:\t", array)
+    end
+end
+
+
+function compute_kernel_ranges()
+    params = ArmonParameters(;
+        :ieee_bits => sizeof(Float64)*8,
+        :test => :Sod, :scheme => :GAD, :projection => :euler_2nd, :riemann_limiter => :minmod,
+        :nghost => 5, :nx => 1, :ny => 1,
+        :cfl => 0,
+        :silent => 5, :write_output => false, :measure_time => false,
+        :use_MPI => false,
+        :single_comm_per_axis_pass => false, :async_comms => false,
+        :use_simd => false, :use_threading => false,
+    )
+    host_array = Vector{Float64}()
+
+    dt = params.Dt
+
+    steps = [
+        # (d, drs) -> (Armon.dtCFL, Armon.dtCFL(params, d, dt)),
+        (d, drs) -> (Armon.update_EOS!, Armon.update_EOS!(params, d, Armon.full_domain(drs), :full)),
+        (d, drs) -> (Armon.boundaryConditions!, Armon.boundaryConditions!(params, d, host_array, params.current_axis)),
+        (d, drs) -> (Armon.numericalFluxes!, Armon.numericalFluxes!(params, d, dt, drs, :full)),
+        (d, drs) -> (Armon.cellUpdate!, Armon.cellUpdate!(params, d, dt, drs)),
+        (d, drs) -> (Armon.projection_remap!, Armon.projection_remap!(params, d, host_array, dt)),
+    ]
+
+    for axis in (Armon.X_axis, Armon.Y_axis)
+        Armon.update_axis_parameters(params, axis)
+        domain_ranges = Armon.compute_domain_ranges(params)
+
+        steps_range = Dict{Symbol, MVector{2, Tuple{Int, Int}}}()
+        current_stencils = nothing
+
+        println("\n$axis:")
+
+        for step in Iterators.reverse(steps)
+            data = imprinting_data(params)
+            func, _ = step(data, domain_ranges)
+            step_name = typeof(func).name.name
+
+            step_range = MVector{2, Tuple{Int, Int}}((0, 0), (0, 0))
+
+            if isnothing(current_stencils)
+                # Init for the last step
+                steps_range[step_name] = step_range
+                current_stencils = data
+                print_data_stencil(data; indent="    ")
+                continue
+            end
+
+            for field in fieldnames(typeof(data))
+                step_stencil = getproperty(data, field)
+                current_stencil = getproperty(current_stencils, field)
+
+                if was_read(current_stencil) && was_written(step_stencil)
+                    # Extend the range of the current step so that it writes to all cells which are read
+                    for d in Base.OneTo(2)
+                        get_range = current_stencil.min_get[d]:current_stencil.max_get[d]
+
+                        f_offset, l_offset = step_range[d]
+                        set_range = (step_stencil.min_set[d] + f_offset):(step_stencil.max_set[d] + l_offset)
+
+                        f_offset += min(0, first(get_range) - first(set_range))
+                        l_offset += max(0, last(get_range) - last(set_range))
+                        step_range[d] = (f_offset, l_offset)
+                    end
+                end
+            end
+
+            for field in fieldnames(typeof(data))
+                step_stencil = getproperty(data, field)
+                current_stencil = getproperty(current_stencils, field)
+
+                f_offsets = first.(step_range)
+                l_offsets = last.(step_range)
+                step_stencil.min_get .+= f_offsets
+                step_stencil.max_get .+= l_offsets
+                step_stencil.min_set .+= f_offsets
+                step_stencil.max_set .+= l_offsets
+
+                # TODO: sets should cancel gets
+
+                union!(current_stencil, step_stencil)
+            end
+
+            steps_range[step_name] = step_range
+
+            println("  $step_name:")
+            print_data_stencil(data; indent="    ")
+        end
+
+        display(steps_range)
+        println()
+    end
+end
